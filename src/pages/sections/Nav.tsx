1fbb4d8dce8831f05818545208748699f6928737
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-15 00:26:00 +0100

    reaaranged image directory

--- src/pages/sections/Nav.tsx
+++ src/pages/sections/Nav.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion";
 import styles from '../../../styles';
 import { navVariants } from "../../../utils/motion";
+import Image from 'next/image'
 
 const Nav: React.FC = () => {
   return (
@@ -13,9 +14,9 @@ const Nav: React.FC = () => {
     className={`${styles.xPaddings} py-8 relative`}
     >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} max-auto flex justify-between gap-8`}>
-    </div>
-     <img src="../images/showmax.png" alt="logo" /> 
+    <div className={`${styles.innerWidth} max-auto flex justify-between gap-8`} />
+    <div className="flex items-center flex-shrink-0 text-black mr-6" />
+      <Image src="/logo.png" alt="Logo" width={48} height={48} />
     </motion.nav>
   );
 };
