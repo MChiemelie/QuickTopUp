866f93cfb2f2189023bf89fc9856b8521cf7abe2
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-14 20:43:08 +0100

    Co mmitting progress

--- next.config.js
+++ next.config.js
@@ -3,4 +3,32 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+// next.config.js
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/src/:path*',
+        destination: '/src/:path*',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        // Serve all PNG and SVG files with a Cache-Control header of 1 week
+        source: '/(.png|.svg)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=604800, immutable',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+
 module.exports = nextConfig
