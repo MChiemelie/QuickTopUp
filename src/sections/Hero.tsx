1157bf9e547ef455e01d263fc94cd37d33a0fdc7
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-18 01:18:08 +0100

    from TS to JS

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,18 +5,18 @@ const Hero: React.FC = () => {
     <>
       <h2
       className="
-      my-4 md:my-6 mb-0 p-10 font-sans text-base md:font-bold lg:font-extrabold md:text-6xlg
+      my-4 md:my-6 mb-0 p-10 font-sans md:font-bold lg:font-extrabold md:text-6xlg
       text-sky-100 text-7xl text-center 
       bg-[url('../../public/map.png')]  
       bg-cover bg-no-repeat bg-center"
-      >QuickTop is Nigeria's No 1 Platform for Airtime, Data, TV subscriptions.
-      </h2>
-      <p
+      >QuickTopUp is Nigerians' No 1 Platform for Airtime, Data, TV subscriptions.
+       <p
       className='
-      m-4 md:m-8 p-10 font-sans text-base md:font-slim lg:font-semibold md:text-6xlg
-      text-sky-100 text-xl text-center 
+      m-1 md:m-2 p-8 font-sans md:font-thin lg:font-semibold md:text-6xlg
+      text-sky-100 text-base text-center opacity-80
       bg-cover bg-no-repeat bg-center
       '>a leading platform in Nigeria that offers a convenient and hassle-free way to purchase airtime, data, and TV subscriptions.</p>
+      </h2>
     </>
   );
 };
