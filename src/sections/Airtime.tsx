5c5aa46a0b50ba5c9043a0d8df4dd0ad68c830c7
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-19 20:57:42 +0100

    Removed src folder.

--- src/sections/Airtime.tsx
+++ src/sections/Airtime.tsx
@@ -3,12 +3,27 @@ import React from 'react';
 const Airtime: React.FC = () => {
   return (
     <>
-      <h1
-      // className="
-      // m-1 md:m-2 p-8 font-sans md:font-bold lg:font-semibold md:text-6xlg
-      // text-sky-100 text-4xl text-left
-      // bg-cover bg-no-repeat bg-center"
-      >This is the airtime section </h1>
+    <div
+    className=''></div>
+    <div
+    className='w-1/2 justify-items-start'>
+    <h1
+      className="
+      m-1 md:m-2 px-8 font-sans md:font-bold lg:font-semibold md:text-6xlg
+      text-sky-100 text-4xl text-left
+      bg-cover bg-no-repeat bg-center"
+      >Buy airtime</h1>
+      <p
+      className="
+      m-1 md:m-2 px-8 font-sans md:text-6xlg
+      text-sky-100 text-xl text-left
+      bg-cover bg-no-repeat bg-center"
+      >Recharge to all your favourite networks.</p>
+    </div>
+    <div
+    className='w-1/2 justify-items-end'>
+      
+    </div>
     </>
   );
 };
