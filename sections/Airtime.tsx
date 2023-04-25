20eaffdb2cac9fb5d9af909267d83375167e5948
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-25 21:05:13 +0100

    Airtime

--- sections/Airtime.tsx
+++ sections/Airtime.tsx
@@ -16,19 +16,20 @@ const Airtime: React.FC = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: .9}}
-      className='flex justify-between bg-black'>
+      className='flex justify-around bg-black'>
     <div>
     <motion.h1
       variants={textVariant(1.5)}
       className='my-8 font-sans text-sky-100 
-      leading-normal tracking-wide'
-      >Buy airtime</motion.h1>
+      leading-normal tracking-wide text-base font-semibold
+      sm:text-lg md:text-2xl lg:text-4xl 2xl:text-6xl'
+      >Buy airtime and data</motion.h1>
       <motion.p
       variants={textVariant2}
       className="
       font-sans justify-center
       text-sky-100 text-left
-      text-base"
+      sm:text-base md:text-lg lg:text-xl 2xl:text-2xl"
       >Recharge all your favourite networks at very discounted prices.
       </motion.p>
     </div>
@@ -40,7 +41,8 @@ const Airtime: React.FC = () => {
       >
         <motion.div
           variants={airtimeItem}
-          className='grid grid-rows-2 grid-flow-col  gap-2 rounded hover:scale-75 sm:w-24 md:32 2xl:w-40'>
+          className='grid grid-rows-2 grid-flow-col  gap-2 rounded
+          hover:scale-75 sm:w-36 md:40 2xl:w-48'>
            <Image src={mtn} alt='mtn'/>
            <Image src={glo} alt='glo' />
            <Image src={airtel} alt='airtel'/>
