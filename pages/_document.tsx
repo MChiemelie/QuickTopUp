1c53211988914ae70df1d6b8360f506241ab51d8
Melikam Chukwuemelie John <melikamchukwuemelie@gmail.com>
2023-04-29 04:09:16 +0100

    Working on the fonts.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin="anonymous"/>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet"></link>
       <body>
         <Main />
         <NextScript />
